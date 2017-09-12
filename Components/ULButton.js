672884cab00c1143b9c00347584ebe45431bd777
Ulna <hello@ulna.co>
2017-09-11 19:55:55 -0500

    base

---
+++ Components/ULButton.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+
+export class ULButton extends React.Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <View>
+        <View>
+          <Text>Test</Text>
+        </View>
+      </ View>
+    )
+  }
+}
